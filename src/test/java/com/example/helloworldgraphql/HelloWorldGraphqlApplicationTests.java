88e08b7bc016ffc03752bdf8418052d0c859dfdf
Júlio Cezar Koronati <jckoronati@hotmail.com>
2023-09-12 21:22:42 -0300

    first commit

---
+++ src/test/java/com/example/helloworldgraphql/HelloWorldGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
